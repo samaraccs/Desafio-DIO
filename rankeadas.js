71a4022f6f75ec5ac840b08c4df6c96dfba89aa6
samaraccs <samaracsilva95@gmail.com>
2025-07-16 21:25:10 -0300

    segundo desafio

---
+++ rankeadas.js
@@ -0,0 +1,35 @@
+//Desafio DIO 2 - partidas Rankeadas
+let qtdeVitorias = 100
+let qtdeDerrotas = 20
+
+function quantidadeFinal(){
+    let qtdeFinal = qtdeVitorias - qtdeDerrotas
+    return qtdeFinal
+}
+
+let saldoRankeadas = quantidadeFinal()
+let nivel = ""
+
+if(saldoRankeadas <= 10){
+    nivel = "Ferro"
+  }
+    else if(saldoRankeadas <= 20){
+    nivel = "Bronze"
+ }
+    else if(saldoRankeadas <= 50){
+    nivel = "Prata"
+ }
+  else if(saldoRankeadas <= 80){
+    nivel = "Ouro" 
+}
+   else if(qsaldoRankeadas <= 90){
+    nivel = "Diamante"
+}
+    else if(quantidadeXP <= 100){
+    nivel = "Lendário"
+}
+    else{
+    nivel = "Imortal"
+ }
+
+ console.log("O herói tem saldo " + saldoRankeadas +  " e está no nível... " + nivel)
